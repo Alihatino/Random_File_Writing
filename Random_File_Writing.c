b724b940a10a9c5d8c9d9fd80554311251c7028c
DrGrunwalder <109179244+Alihatino@users.noreply.github.com>
2023-08-22 12:43:28 +0300

    Update Random_File_Writing.c

--- Random_File_Writing.c
+++ Random_File_Writing.c
@@ -21,15 +21,18 @@ else{
 
   while (client.acctNum! = 0){
     printf("%s","Enter lastname,firstname,balance\n?");
-    fscanf( stdin,
-      
+    fscanf( stdin, "%14s%9s%1f",client.lastName,client.firstName,&client.balance);
 
 
+      fseek(cfPtr,(client.acctNum-1)*
+          sizeof(struct clientData), SEEK_SET);
 
+      fwrite (&client , sizeof(struct clientData) , 1 , cfPtr);
+      printf("%s","enter account number\n?");
+      scanf("%d",&client.acctNum);
+  }
 
+fclose(cfPtr);
 
-
-
-
-
+}
 }
